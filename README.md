e5407f9ab38b50ad85ffefcb38fa011701af5a75
Anatoly Dudko <anatoly_dudko@icloud.com>
2024-05-04 18:17:52 +0200

    init application

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Personal Journal</h1>
+
+
+<p>Backend: <strong>Spring Boot + Java</strong></p>
+<p>Database: <strong>Postgres</strong></p>
---
+++ server/src/main/java/net/dudko/project/MoneronApplication.java
@@ -0,0 +1,13 @@
+package net.dudko.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneronApplication.class, args);
+    }
+
+}
