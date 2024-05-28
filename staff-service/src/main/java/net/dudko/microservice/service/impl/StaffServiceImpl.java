de8b5873c028d1c4f207f9b359f28611600a199c
Anatoly Dudko <anatoly_dudko@icloud.com>
2024-05-28 14:37:31 +0200

    added metrics with zipkin, breaker and retry

--- staff-service/src/main/java/net/dudko/microservice/service/impl/StaffServiceImpl.java
+++ staff-service/src/main/java/net/dudko/microservice/service/impl/StaffServiceImpl.java
@@ -1,9 +1,11 @@
 package net.dudko.microservice.service.impl;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import net.dudko.microservice.domain.mapper.StaffMapper;
 import net.dudko.microservice.domain.repository.StaffRepository;
 import net.dudko.microservice.model.dto.ApiResponseDto;
+import net.dudko.microservice.model.dto.DepartmentDto;
 import net.dudko.microservice.model.dto.StaffDto;
 import net.dudko.microservice.model.dto.StaffStatus;
 import net.dudko.microservice.model.exception.ResourceDuplicatedException;
@@ -11,8 +13,6 @@ import net.dudko.microservice.model.exception.ResourceNotFoundException;
 import net.dudko.microservice.service.DepartmentServiceApiClient;
 import net.dudko.microservice.service.StaffService;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -22,8 +22,6 @@ public class StaffServiceImpl implements StaffService {
 
     private final StaffRepository staffRepository;
 
-//    private final RestTemplate restTemplate;
-//    private final WebClient webClient;
     private final DepartmentServiceApiClient departmentServiceApiClient;
 
     @Override
@@ -36,30 +34,15 @@ public class StaffServiceImpl implements StaffService {
         return StaffMapper.mapToStaffDto(inDb);
     }
 
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public ApiResponseDto getById(Long id) {
         var inDb = staffRepository
                 .findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee with id %s not found!", id)));
-
-//        // Call department-service with restTemplate
-//        var inDepartmentService = restTemplate.getForEntity(
-//                "http://localhost:8081/department/code/" + inDb.getDepartmentCode(),
-//                DepartmentDto.class);
-
-//        // Call department-service with webClient
-//        var inDepartmentService = webClient.get()
-//                .uri( "http://localhost:8081/department/code/" + inDb.getDepartmentCode())
-//                .retrieve()
-//                .bodyToMono(DepartmentDto.class)
-//                .block();
-
-        // Call department-service with openFeign
         var inDepartmentService = departmentServiceApiClient.getDepartmentByCode(inDb.getDepartmentCode());
-
         return ApiResponseDto.builder()
                 .staffDto(StaffMapper.mapToStaffDto(inDb))
-//                .departmentDto(inDepartmentService.getBody())
                 .departmentDto(inDepartmentService)
                 .build();
     }
@@ -83,6 +66,21 @@ public class StaffServiceImpl implements StaffService {
                 .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee with id %s not found!", id)));
         inDb.setStatus(StaffStatus.DELETED);
         staffRepository.save(inDb);
+    }
+
+    public ApiResponseDto getDefaultDepartment(Long id, Exception exception) {
+        var inDb = staffRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee with id %s not found!", id)));
+        var inDepartmentService = DepartmentDto.builder()
+                .name("Default Department")
+                .description("You see this department, because department-service not available. Contact Support")
+                .code("DF")
+                .build();
+        return ApiResponseDto.builder()
+                .staffDto(StaffMapper.mapToStaffDto(inDb))
+                .departmentDto(inDepartmentService)
+                .build();
     }
 
 }
